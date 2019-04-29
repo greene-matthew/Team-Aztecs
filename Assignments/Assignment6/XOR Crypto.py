1d8d1e51a6cddebd4670922847cca9283f8e3c29
unknown <greenematthew1234@gmail.com>
2019-04-29 17:46:10 -0500

    Commented out print statments

--- Assignments/Assignment6/XOR Crypto.py
+++ Assignments/Assignment6/XOR Crypto.py
@@ -6,21 +6,21 @@ message = open("ciphertext2", "rb")
 
 compare1 = bin(int(binascii.hexlify(message.read()), 16))
 compare2 = bin(int(binascii.hexlify(key.read()), 16))
-print(len(compare1))
-print(len(compare2))
+#print(len(compare1))
+#print(len(compare2))
 message.seek(0, os.SEEK_END)
-print(message.tell())
+#print(message.tell())
 
 key.seek(0, os.SEEK_END)
-print(key.tell())
+#print(key.tell())
 sub = len(compare2) - len(compare1)
-print sub
+#print sub
 
-print(len(compare2))
+#print(len(compare2))
 binaryString = int(compare1, 2) ^ int(compare2, 2)
 
 real = bin(binaryString)
-print len(real)
+#print len(real)
 returnString = int(real, 2)
 pic = binascii.unhexlify('%x' % returnString)
-print pic
+#print pic
