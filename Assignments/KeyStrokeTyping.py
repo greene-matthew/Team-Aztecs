82288cbb85c17d181d390fe96cd47fb11ef5abd5
unknown <greenematthew1234@gmail.com>
2019-05-09 23:06:53 -0500

    Here is keyStroke

--- Assignments/KeyStrokeTyping.py
+++ Assignments/KeyStrokeTyping.py
@@ -1,3 +1,9 @@
+from pynput.keyboard import Key, Controller
+from time import sleep
+from random import uniform
+from termios import tcflush, TCIFLUSH
+from sys import stdin, stdout
+
 password = raw_input()
 timings = raw_input()
 
@@ -5,11 +11,27 @@ password = password.split(",")
 password = password[:len(password) / 2 +1]
 password = "".join(password)
 
-print password
+
 timings = timings.split(",")
 timings = [float(a) for a in timings]
 keypress = timings[:len(timings)/2 +1]
 keyinterval = timings[len(timings)/2 +1:]
+keyboard = Controller()
 
-print "key press times = {}".format(keypress)
-print "key intervals = {}".format(keyinterval)
+
+string = "This is supposed to be a fake string"
+i = 0
+
+for char in password:
+    
+    keyboard.press(char)
+    sleep(keypress[i])
+    
+    keyboard.release(char)
+    if(i == len(keypress)-1): break
+    sleep(keyinterval[i])
+    i = i+1
+try:
+    tcflush(stdin, TCIFLUSH)
+except:
+    print
